2dbd0790db6850a62bf23c5be720892bafb563c9
Thomas Cook <trcook@gmail.com>
2016-02-09 21:25:38 -0700

    minor changes, new options for directory convert functions

--- tmisc/R/convert_rda.R
+++ tmisc/R/convert_rda.R
@@ -1,18 +1,22 @@
 #' convertdta
 #' @description
 #' converts files in directory from dta to rda and saves copies. 
+#' Among other things, this is a useful function to use if you need 
+#' or want to save some harddrive space.
 #'@export convertdta
 convertdta<-function(dir="./"){
 	require(foreign)
+  save_method<-switch(save_format,'rda'=save,'rds'=saveRDS)
+  ext=paste0('.',save_format,sep='')
 	to_proc<-dir(dir,pattern="*.dta")
 	to_proc<-data.frame(filename=to_proc,filepath=file.path(dir,to_proc,fsep=''),objname=gsub(to_proc,pattern='\\.dta|-|\\s',replacement=''))
-	to_proc$newpath<-gsub(x=to_proc$filepath,pattern='\\.dta',rep='.rda')
+	to_proc$newpath<-gsub(x=to_proc$filepath,pattern='\\.dta',rep=ext)
 #	print(gsub(x=to_proc$filepath,pattern='\\.dta',rep='.rda'))
 	print(to_proc)
 	apply(to_proc,1,function(x){
 		eval(substitute({
 			obj<-read.dta(filepath)
-			save(obj,file=newpath)
+			save_method(obj,file=newpath)
 			},
 			env=list(obj=as.name(x['objname']),filepath=x['filepath'],newpath=x['newpath'])))
 		})
@@ -21,7 +25,8 @@ convertdta<-function(dir="./"){
 
 #' convertcsv
 #' @description
-#' converts files in directory from dta to rda and saves copies. 
+#' converts files in directory from dta to rda and saves copies.
+#' Among other things, this is a useful function to use if you need #' or want to save some harddrive space. 
 #'@export convertcsv
 convertcsv<-function(dir="./",save_format='rda'){
 	require(foreign)
@@ -46,13 +51,15 @@ convertcsv<-function(dir="./",save_format='rda'){
 #' convertxls
 #' @description
 #' converts files in directory from xls to rda and saves copies. 
+#' Among other things, this is a useful function to use if you need #' or want to save some harddrive space.
 #' @note 
 #' You should/must put a trailing slash on the end of the path you are searching. 
 #'@export convertxls
 
-convertxls<-function(mydir="./"){
+convertxls<-function(mydir="./",save_format='rda'){
   # to_proc<-dir(mydir,pattern="*.xls|*.xlsx",recursive=T,all.files = FALSE)
-  
+  save_method<-switch(save_format,'rda'=save,'rds'=saveRDS)
+  ext=paste0('.',save_format,sep='')
   # Step 1: get xls files in directory
   to_proc<-dir(
   mydir,pattern="*.xls|*.xlsx",recursive=T,
@@ -64,7 +71,9 @@ convertxls<-function(mydir="./"){
   workbooks<-list()
   for(i in to_proc){
     ifile<-i
-    objname<-gsub(x=i,pattern='\\.xlsx|\\.xls|-|\\s|[^a-zA-Z0-9]',replacement='')
+    objname<-gsub(x=i,pattern='\\.xlsx|\\.xls',replacement='')
+    objname<-gsub(x=i,pattern='-|\\s|[^a-zA-Z0-9]',replacement='_')
+    
     workbooks[[objname]]<-XLConnect::loadWorkbook(filename = file.path(mydir,ifile,fsep=''))
   }
   
@@ -81,12 +90,12 @@ convertxls<-function(mydir="./"){
       print("multiple")
     for(i in sht){
             print("forbreak")
-      outname<-paste(names(workbooks)[[w]],i,'.RDS',sep='')
+      outname<-paste(names(workbooks)[[w]],i,ext,sep='')
       print(outname)
-      saveRDS(x[i],file = file.path(mydir,outname))
+      save_method(x[i],file = file.path(mydir,outname))
     }}else{
       outname<-paste(x,'.RDS',sep='')
-      saveRDS(x,file=file.path(mydir,outname))
+      save_method(x,file=file.path(mydir,outname))
     }})
     return()}
     
